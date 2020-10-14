445f2a25e690006d9761bd5de3b3078007eb0af9
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-14 07:23:18 +0530

    Add : Welcome message

---
+++ MyLinkedList/src/main/java/com/capgemini/myLinkedList/MyNode.java
@@ -0,0 +1,19 @@
+package com.capgemini.myLinkedList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+public class MyNode 
+{
+
+	
+	private static final Logger log = LogManager.getLogger(MyNode.class);
+    public static void main( String[] args )
+    {
+    	
+        log.info( "Welcome" );
+      
+    }
+}
---
+++ MyLinkedList/src/test/java/com/capgemini/myLinkedList/MyNodeTest.java
@@ -0,0 +1,20 @@
+package com.capgemini.myLinkedList;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MyNodeTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
